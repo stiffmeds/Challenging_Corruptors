fe9fd14084d57ce02db8b3b86d31a3547f912346
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-09-06 13:34:11 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace Challenging_Corruptors
+{
+    [BepInPlugin("com.meds.challengingcorruptors", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new("com.meds.challengingcorruptors");
+        internal static ManualLogSource Log;
+        public static ConfigEntry<int> medsMinimumCorruptor { get; private set; }
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = Logger;
+            medsMinimumCorruptor = Config.Bind("General", "Minimum Corruptor Difficulty", 3, new ConfigDescription("1: easy (vanilla); 2: average; 3: hard; 4: extreme", new AcceptableValueRange<int>(1, 4)));
+            medsMinimumCorruptor.SettingChanged += (obj, args) => { Challenging_Corruptors.UpdateChallengeCorruptors(); };
+            harmony.PatchAll();
+            Logger.LogInfo($"Challenging Corruptors enabled!");
+        }
+    }
+    [HarmonyPatch]
+    public class Challenging_Corruptors
+    {
+        public static List<string> medsCorruptors = new();
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Globals), "CreateGameContent")]
+        public static void CreateGameContentPostfix()
+        {
+            medsCorruptors = Globals.Instance.CardListByType[Enums.CardType.Corruption];
+            UpdateChallengeCorruptors();
+        }
+
+        public static void UpdateChallengeCorruptors()
+        {
+            Globals.Instance.CardListByType[Enums.CardType.Corruption] = new();
+            foreach (string cor in medsCorruptors)
+            {
+                CardData card = Globals.Instance.GetCardData(cor);
+                if (card != (CardData)null && ((card.CardRarity == Enums.CardRarity.Common && Plugin.medsMinimumCorruptor.Value == 1) || (card.CardRarity == Enums.CardRarity.Uncommon && Plugin.medsMinimumCorruptor.Value <= 2) || (card.CardRarity == Enums.CardRarity.Rare && Plugin.medsMinimumCorruptor.Value <= 3) || (card.CardRarity == Enums.CardRarity.Epic)))
+                    Globals.Instance.CardListByType[Enums.CardType.Corruption].Add(cor);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Challenging Corruptors
+
+Sick of getting Easy corruptors? Want a challenge? Challenging Corruptors allows you to choose the minimum difficulty of your corruptors.
+
+Defaults to only Hard and Extreme corruptors; after first run a config file is created in your BepInEx _config_ folder that allows you to change the minimum corruptor difficulty.
+
+## Installation
+
+Install [BepInEx](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+
+Download the [latest release](https://github.com/stiffmeds/Challenging_Corruptors) and put it in your BepInEx _plugins_ folder.
+
+## Support
+
+Raise a Github issue or ping @stiffmeds in the #modding channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
+
+## Donations
+
+I make mods because I love it, not because I want to make money from it. If you really, really want to donate to me, my preferred non-profit organisations are [Greyhound Adoptions WA](https://greyhoundadoptionswa.com.au/donation/) ([paypal](https://www.paypal.com/donate?token=m8DwEGGEH0FFsS6PS-5p4MX9_5g8_ocMMrNFjaELN-xcG6Ok-KCFabu5xtB-57QBiOM7QLSuKVUepvL_)) or [Headache Australia](https://headacheaustralia.org.au/donate/).
